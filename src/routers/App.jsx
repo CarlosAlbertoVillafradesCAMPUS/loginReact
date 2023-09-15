3665f2a80cc752533643104cc4ce122d5a51ba0e
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-09-14 22:18:01 -0500

    feat: :sparkles: modificacio home
    se modifico el la consulta del home para que cada vez que se acceciera al home este realizara la consulta a la api y trayera la informacion del usuarios que se habia logueado, ademas de agregarle un botton para volver al logue
    
    by: Carlos villafrades

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -1,15 +1,40 @@
-import React from "react"
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useLocation } from "react-use";
+import ProtectedRoute from "../utils/ProtectedRoute";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 
 function App() {
+  const [activateLogin, setActivateLogin] = useState(true)
+  const [activatePages, setActivatePages] = useState(false)
+  const locationRoute = useLocation()
+
+  const activate = () =>{
+    const infoLocalStorage = localStorage.getItem("token")
+    if (infoLocalStorage) {
+      setActivateLogin(false)
+      setActivatePages(true)
+    }else{
+      setActivateLogin(true)
+      setActivatePages(false)
+    }
+  }
+
+  useEffect(() => {
+    activate();
+  }, [locationRoute]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+      <Route element={<ProtectedRoute canActivate={activateLogin} redirectPath="/home" />}>
+          <Route path="/" element={<Login />} />
+      </Route>
+      <Route element={<ProtectedRoute canActivate={activatePages} redirectPath="/" />}>
+          <Route path="/home" element={<Home />} />
+      </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
