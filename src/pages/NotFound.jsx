3223ede799599f603b72f1bde1cd82b7837c2b05
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2023-09-24 22:52:11 -0500

    feat: :lipstick: Agregaron estilos al NOT FOUND
    se le agregaron estilos y una pequeña imagen  La pagina de not found
    
    by: Carlos Villafrades

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import robot from "../assets/robotVerde-removebg-preview.png"
+import "../styles/Home.css"
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
-    <div>ERROR 404. Page Not Found</div>
+    <div className='principalNot'>
+    <div className='infoHome'>
+      <div className='contTextError'>
+      <div>
+        <img src={robot} />
+      </div>
+        <p className='textError'>ERROR 404</p>
+        <p className='notFound'>Page Not Found</p>
+      </div>
+      <br />
+      <br />
+      <div className='containerButtonHome'>
+      <Link to={"/"} className='volver'>Regresar</Link>
+      </div>
+    </div>
+    </div>
   )
 }
